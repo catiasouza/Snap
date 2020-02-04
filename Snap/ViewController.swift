1dad1d5c48581dd8d0560ebebdbbd11690821beb
catiamiranda <catiamiranda@brq.com>
2020-02-03 23:08:52 -0300

    Teste

--- Snap/ViewController.swift
+++ Snap/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let someImageView: UIImageView = {
+        let theImageView = UIImageView()
+        theImageView.image = UIImage(named: "chucknorris")
+        theImageView.translatesAutoresizingMaskIntoConstraints = false //You
+        return theImageView
+    }()
+    
+    lazy var box = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
+        view.addSubview(someImageView)
+        someImageViewConstraints()
+       
+        //LABEL
+        //        let childView = UIView()
+        //        childView.backgroundColor = .blue
+        //        self.view.addSubview(childView)
+        //
+        //        childView.snp.makeConstraints { (make) in
+        //            make.size.equalTo(CGSize(width: 300, height: 100))
+        //            make.top.equalTo(self.view.snp.top).offset(800)
+        //            make.centerX.equalTo(self.view)
+        //        }
+        
+        
+        //BOTAO
+        self.view.addSubview(box)
+        box.backgroundColor = .orange
+        
+        
+        box.snp.makeConstraints { (make) -> Void in
+            make.center.equalTo(self.view)
+            make.centerY.equalTo(800)
+            make.size.equalTo(CGSize(width: 250, height: 75))
+            
+            
+        }
+        
     }
-
-
+    //IMAGEM
+    func someImageViewConstraints() {
+        someImageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        someImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        someImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        someImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50).isActive = true
+        
+        
+    }
+    //NAVIGATION
+    func setup(){
+        
+        
+        view.backgroundColor = .white
+        let superview = self.view
+        let navbar = UIView()
+        superview!.addSubview(navbar)
+        navbar.backgroundColor = UIColor.lightGray
+        navbar.snp_makeConstraints { (make) -> Void in
+            make.height.equalTo(90)
+            make.width.equalTo(superview!)
+        }
+        let titleLabel = UILabel()
+        navbar.addSubview(titleLabel)
+        titleLabel.text = "9999999"
+        titleLabel.textColor = UIColor.black
+        titleLabel.sizeToFit()
+        titleLabel.snp_makeConstraints { (make) -> Void in
+            
+        }
+        
+        
+    }
+    
+    
 }
-
